88bcb9dcf50738bba4292143ae78dc555377c34e
anmrdz <anmrdz@gmail.com>
2015-10-20 23:32:24 -0500

    Agregadas clases Forum y Post

---
+++ forum/grails-app/domain/forum/Forum.groovy
@@ -0,0 +1,15 @@
+package forum
+
+class Forum{
+
+String name
+Date dateCreated
+String category
+
+static constraints = {
+  name nullable: false, size: 3..20, unique: true
+  dateCreated nullable: false, min: new Date()
+  category nullable: false, size: 3..15
+}
+
+}
