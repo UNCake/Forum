5bacbed6a2aaa15b120f2aa0c9b7016823eca88d
anmrdz <anmrdz@gmail.com>
2015-10-22 00:10:24 -0500

    Pruebas de unidad File

--- forum/test/unit/forum/FileSpec.groovy
+++ forum/test/unit/forum/FileSpec.groovy
@@ -15,6 +15,49 @@ class FileSpec extends Specification {
     def cleanup() {
     }
 
-    void "test something"() {
+    void "File debe comprobar tipo de archivo"() {
+
+        when: 'archivo tipo algo/si'
+        def p = new File(fileType: 'algo/si', content: [001], size: 3)
+
+        then: 'La validacion deber ser correcta'
+        p.validate()
+
+        when: 'tipo de archivo invalido'
+        p = p = new File(fileType: 'algo', content: [001], size: 3)
+
+        then: 'La validacion debe ser incorrecta'
+        !p.validate()
     }
+
+    void "File debe exigir el campo content"() {
+
+        when: 'content asignado'
+        def p = new File(fileType: 'algo/si', content: [001], size: 3)
+
+        then: 'La validacion deber ser correcta'
+        p.validate()
+
+        when: 'content nulo'
+        p = p = new File(fileType: 'algo', size: 3)
+
+        then: 'La validacion debe ser incorrecta'
+        !p.validate()
+    }
+
+    void "size no debe superar los 10MB"() {
+
+        when: 'size de 7 MB'
+        def p = new File(fileType: 'algo/si', content: [001], size: 7000)
+
+        then: 'La validacion deber ser correcta'
+        p.validate()
+
+        when: 'size de 70 MB'
+        p = p = new File(fileType: 'algo', size: 7000)
+
+        then: 'La validacion debe ser incorrecta'
+        !p.validate()
+    }
+
 }
