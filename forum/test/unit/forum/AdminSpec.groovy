acd04d0a65882c3b3278f0219916f4234170d56a
anmrdz <anmrdz@gmail.com>
2015-10-21 23:58:48 -0500

    Pruebas de unidad Admin

--- forum/test/unit/forum/AdminSpec.groovy
+++ forum/test/unit/forum/AdminSpec.groovy
@@ -10,11 +10,47 @@ import spock.lang.Specification
 class AdminSpec extends Specification {
 
     def setup() {
+        mockForConstraintsTests(User)
+        mockForConstraintsTests(Admin)
     }
 
     def cleanup() {
     }
 
-    void "test something"() {
+    void "El administrador debe tener un nivel de  1 a 5"() {
+
+        when: 'Administrador nivel 3'
+        def p = new Admin(level: 3, rating: 5.0, name: 'Juan Sebastian', age: 14, lastname: 'Gutierrez',
+                password: '1234Adfdd', username: 'jsgutierrez2')
+
+
+        then: 'La validacion deber ser correcta'
+        p.validate()
+
+        when: 'Administrador nivel 0'
+        p = new Admin(level: 0, rating: 5.0, name: 'Juan Sebastian', age: 14, lastname: 'Gutierrez',
+                password: '1234Adfdd', username: 'jsgutierrez2')
+
+        then: 'La validacion debe ser incorrecta'
+        !p.validate()
     }
+
+    void "El administrador debe tener un raing de  0 a 100"() {
+
+        when: 'Administrador rating 3'
+        def p = new Admin(level: 3, rating: 5.0, name: 'Juan Sebastian', age: 14, lastname: 'Gutierrez',
+                password: '1234Adfdd', username: 'jsgutierrez2')
+
+
+        then: 'La validacion deber ser correcta'
+        p.validate()
+
+        when: 'Administrador rating 500'
+        p = new Admin(level: 3, rating: 500.0, name: 'Juan Sebastian', age: 14, lastname: 'Gutierrez',
+                password: '1234Adfdd', username: 'jsgutierrez2')
+
+        then: 'La validacion debe ser incorrecta'
+        !p.validate()
+    }
+
 }
